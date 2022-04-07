441ac1fb212715f100195f246114d8f0d6a9259c
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-07 14:20:32 -0400

    add API post

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,20 +1,83 @@
+const fs = require('fs');
 const router = require('express').Router();
+//As suggested by npm for accessing random ID
+const { v4: uuidv4 } = require('uuid');
+
 // const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
 const { notes } = require('../../db/db.json');
 
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/notes', (req, res) => {
+    fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+        if(err) {
+            console.log("Unable to find notes.");
+            res.json(err);
+        } else {
+            const notes = JSON.parse(data);
+            if (!notes)
+            notes= [];
+            res.json(notes);
+        }
+    })
+});
 
 // POST /api/notes should receive a new note to save on the request body, 
 // add it to the db.json file, and then return the new note to the client. 
-// You'll need to find a way to give each note a unique id when it's saved 
-// (look into npm packages that could do this for you).
+router.post('/notes', (req, res) => {
+    // Destructuring to get title and text
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        //Unique ID
+        review_id: uuidv4(),
+      };
+  
+      fs.readFile(`./db/db.json`, (err, data) => {
+        if(err) {
+        console.log("Unable to access notes.");
+        res.json(err);
+        } else {
+          const notes= JSON.parse(data);
+          console.log("DATA: " + data);
+          if (!notes) {
+              notes = [];
+              console.log("this is your first note");
+          }
+          notes.push(newNote);
+          // Convert the data to a string so we can save it
+          const notePackage = JSON.stringify(notes);
+  
+            fs.writeFile(`./db/db.json`, notePackage, (err) => {
+              if(err)
+              {
+                console.log("Unable to store note.")
+                res.json(err);
+              } else {
+                const response = {
+                  status: 'sucess',
+                  body: newNote,
+                };
+                console.log(response);
+                res.json(response);
+              }
+            }
+            )
+          }
+      })
+  
+    } else {
+        console.log("Note not properly filled out")
+        res.json(err);
+    }
 
-
-
-router.get('/notes', (req, res) => {
-    // res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
+
   
 // router.get('/animals/:id', (req, res) => {
 //     const result = findById(req.params.id, animals);
@@ -24,17 +87,8 @@ router.get('/notes', (req, res) => {
 //       res.send(404);
 //     }
 // });
-  
-router.post('/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-  
-    if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
-    } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
-    }
-});
+
+
+
 
 module.exports  = router;
